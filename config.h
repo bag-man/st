263896b95c3f04e9b083c10ceb38e8b6065a407a
Owen Garland <garland.owen@gmail.com>
2020-11-10 10:24:09 +0100

    feat: bump up to 11 for higher res displays

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "AnonymousProMinus:size=9:antialias=true:autohint=true";
+static char font[] = "AnonymousProMinus:size=11:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
